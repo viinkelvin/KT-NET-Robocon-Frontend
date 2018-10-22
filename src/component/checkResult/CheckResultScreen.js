03c988183cb05a5b10f1b4cf44729a1c88db414d
Unknown <talles_of_destiny@yahoo.com.sg>
2018-10-23 08:06:24 +0900

    Finished check result

--- src/component/checkResult/CheckResultScreen.js
+++ src/component/checkResult/CheckResultScreen.js
@@ -3,8 +3,10 @@ import { StyleSheet, Text, Image, View, Alert, TextInput } from 'react-native';
 // import { strings } from './localization';
 import api from '../../lib/api';
 import ImageButton from '../../shared/ImageButton';
+import Header from '../../shared/Header';
 import { localizationStrings } from './localization';
-
+import { Container, Content, Card, CardItem, Body } from "native-base";
+import Dimensions from 'Dimensions';
 
 export default class CheckResultScreen extends React.Component {
   constructor() {
@@ -25,94 +27,185 @@ export default class CheckResultScreen extends React.Component {
     .catch(err => console.log(err, "error"));
   }
 
+  _renderProfile(data) {
+    return(
+      <View style={styles.table}>
+        <View style={styles.tr}>
+          <View style={styles.tdText}><Text>{data.last_name ? data.last_name.text : '' }</Text></View>
+          <View style={styles.tdValue}><Text>: {data.last_name ? data.last_name.value : '' }</Text></View>
+        </View>
+        <View style={styles.tr}>
+          <View style={styles.tdText}><Text>{data.first_name ? data.first_name.text : '' }</Text></View>
+          <View style={styles.tdValue}><Text>: {data.first_name ? data.first_name.value : '' }</Text></View>
+        </View>
+        <View style={styles.tr}>
+          <View style={styles.tdText}><Text>{data.user_code ? data.user_code.text : '' }</Text></View>
+          <View style={styles.tdValue}><Text>: {data.user_code ? data.user_code.value : '' }</Text></View>
+        </View>
+        <View style={styles.tr}>
+          <View style={styles.tdText}><Text>{data.address ? data.address.text : '' }</Text></View>
+          <View style={styles.tdValue}><Text>: {data.address ? data.address.value : '' }</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+  _renderPhsycal(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.physical_measurement ? data.physical_measurement : '' }</Text></View>
+        <View>
+          <View style={styles.col2row}><Text>{data.height ? data.height.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.body_weight ? data.body_weight.text : '' }</Text></View>
+          <View style={[styles.col2row, { height: 60 }]}><Text>{data.abdominal_circumference ? data.abdominal_circumference.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>BMI</Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.height ? data.height.value : '' } cm</Text></View>
+          <View style={styles.col3row}><Text>: {data.body_weight ? data.body_weight.value : '' } Kg</Text></View>
+          <View style={styles.col3row}><Text>: {data.abdominal_circumference ? data.abdominal_circumference.value : '' }</Text></View>
+          <View style={styles.col3row}><Text>: {data.actual_measurement ? data.actual_measurement : ''} {data.self_measurement ? data.self_measurement : ''} {data.self_assessment ? data.self_assessment : ''}</Text></View>
+          <View style={styles.col3row}><Text>: {data.bmi ? data.bmi :''}</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+  getTextPhsycal(val) {
+    if (val == 1) {
+      return "asd"
+    } else if (val == 2) {
+      return "das"
+    } else {
+      return "qwe"
+    }
+  }
+
+  _renderBloodPreasure(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.blood_pressure_measurement ? data.blood_pressure_measurement : '' }</Text></View>
+        <View>
+          <View style={styles.col2row}><Text>{data.systole ? data.systole.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.diastolic_phase ? data.diastolic_phase.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.number_of_measurements ? data.number_of_measurements.text : '' }</Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.systole ? data.systole.value : '' } mmHg</Text></View>
+          <View style={styles.col3row}><Text>: {data.diastolic_phase ? data.diastolic_phase.value : '' } mmHg</Text></View>
+          <View style={styles.col3row}><Text>: {data.number_of_measurements ? data.number_of_measurements.value : '' }</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+ _renderUrinalysis(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.urinalysis ? data.urinalysis : '' }</Text></View>
+        <View>
+          <View style={styles.col2row}><Text>{data.protein ? data.protein.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.sugar ? data.sugar.text : '' }</Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.protein ? data.protein.value : '' } </Text></View>
+          <View style={styles.col3row}><Text>: {data.sugar ? data.sugar.value : '' } </Text></View>
+        </View>
+      </View>
+    );
+  }
+
+  _renderBloodLipids(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.blood_lipids ? data.blood_lipids : '' }</Text></View>
+        <View>
+          <View style={styles.col2row}><Text>{data.neutral_fat ? data.neutral_fat.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.hdl_cholesterol ? data.hdl_cholesterol.text : '' } </Text></View>
+          <View style={styles.col2row}><Text>{data.ldl_cholesterol ? data.ldl_cholesterol.text : '' } </Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.neutral_fat ? data.neutral_fat.value : '' } mg/dl</Text></View>
+          <View style={styles.col3row}><Text>: {data.hdl_cholesterol ? data.hdl_cholesterol.value : '' } mg/dl</Text></View>
+          <View style={styles.col3row}><Text>: {data.ldl_cholesterol ? data.ldl_cholesterol.value : '' } mg/dl</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+  _renderLiver(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.liver_function ? data.liver_function : '' }</Text></View>
+        <View>
+          <View style={styles.col2row}><Text>GOT</Text></View>
+          <View style={styles.col2row}><Text>GPT</Text></View>
+          <View style={styles.col2row}><Text>Y-GPT </Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.got ? data.got : '' } IU/I</Text></View>
+          <View style={styles.col3row}><Text>: {data.gpt ? data.gpt : '' } IU/I</Text></View>
+          <View style={styles.col3row}><Text>: {data.y_gpt ? data.y_gpt : '' } IU/I</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+  _renderDiabetes(data) {
+    return(
+      <View style={[styles.table, {flexDirection: 'row'}]}>
+        <View style={styles.col1}><Text>{data.diabetes_mellitus ? data.diabetes_mellitus : '' }</Text></View>
+        <View>
+         <View style={styles.col2row}><Text>{data.blood_glucose_level ? data.blood_glucose_level.text : '' }</Text></View>
+          <View style={styles.col2row}><Text>{data.hba1c ? data.hba1c.text : '' } </Text></View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <View style={styles.col3row}><Text>: {data.blood_glucose_level ? data.blood_glucose_level.value : '' } mg/dl</Text></View>
+          <View style={styles.col3row}><Text>: {data.hba1c ? data.hba1c.value : '' } %</Text></View>
+        </View>
+      </View>
+    );
+  }
+
+
 	render() {
-    const { data } = this.state;
+    const {width, height} = Dimensions.get('window');
+    const { data, strings } = this.state;
+    const dataContainerHeight = height - 125;
+
 		return(
 			<View style={styles.container}>
-        <Table style={{flexDirection: 'row'}}>
-          {/* Left Wrapper */}
-          <TableWrapper style={{width: 80}}>
-            <Cell data="" style={styles.singleHead}/>
-            <TableWrapper style={{flexDirection: 'row'}}>
-              <Col data={[data.physical_measurement ? data.physical_measurement : '', 'H2']} style={styles.head} heightArr={[60, 60]} textStyle={styles.text} />
-              <Col data={tableTitle} style={styles.title} heightArr={[30, 30, 30, 30]} textStyle={styles.titleText}></Col>
-            </TableWrapper>
-          </TableWrapper>
- 
-          {/* Right Wrapper */}
-          <TableWrapper style={{flex:1}}>
-            <Cols data={tableData} heightArr={[40, 30, 30, 30, 30]} textStyle={styles.text}/>
-          </TableWrapper>
-        </Table>
-
-				<Text style={styles.title}>{this.state.strings.title}</Text>
-				<View style={styles.resultContainer}>
-			    <View style={styles.text}>
-            <Text>{data.last_name ? data.last_name.text : '' } : {data.last_name ? data.last_name.value : '' }</Text>
-            <Text>{data.first_name ? data.first_name.text : '' } : {data.first_name ? data.first_name.value : '' }</Text>
-
-            <Text>{data.user_code ? data.user_code.text : '' } : {data.user_code ? data.user_code.value : '' }</Text>
-            <Text>{data.address ? data.address.text : '' } : {data.address ? data.address.value : '' }</Text>
-
+        <Header
+          title={strings.title}
+          back={() => this.props.navigation.goBack()} />
+				<View style={styles.content}>
+			    <View style={[styles.dataContainer, {height: dataContainerHeight}]}>
+            <Content padder>
+              <Card>
+                <CardItem header bordered>
+                  {this._renderProfile(data)}
+                </CardItem>
+                <CardItem style={{ paddingBottom: 0 }}>
+                  {this._renderPhsycal(data)}
+                </CardItem>
+                <CardItem style={{ paddingTop: 0, paddingBottom: 0 }} >
+                  {this._renderBloodPreasure(data)}
+                </CardItem>
+                <CardItem style={{ paddingTop: 0, paddingBottom: 0 }} >
+                  {this._renderUrinalysis(data)}
+                </CardItem>
+                <CardItem style={{ paddingTop: 0, paddingBottom: 0 }} >
+                  {this._renderBloodLipids(data)}
+                </CardItem>
+                <CardItem style={{ paddingTop: 0, paddingBottom: 0 }} >
+                  {this._renderLiver(data)}
+                </CardItem>
+                <CardItem style={{ paddingTop: 0, paddingBottom: 0 }} >
+                  {this._renderDiabetes(data)}
+                </CardItem>
+              </Card>
+            </Content>
           </View>
-          <View>
-            <Text>{data.physical_measurement ? data.physical_measurement : '' }</Text>
-            <View>
-              <Text>{data.height ? data.height.text : '' } : {data.height ? data.height.value : '' } cm</Text>
-              <Text>{data.body_weight ? data.body_weight.text : '' } : {data.body_weight ? data.body_weight.value : '' } kg</Text>
-              <Text>{data.abdominal_circumference ? data.abdominal_circumference.text : '' } : {data.abdominal_circumference ? data.abdominal_circumference.value : '' } </Text>
-              <Text>{data.actual_measurement ? data.actual_measurement : ''}</Text>
-              <Text>{data.self_measurement ? data.self_measurement : ''}</Text>
-              <Text>{data.self_assessment ? data.self_assessment : ''}</Text>
-              <Text>BMI : {data.bmi ? data.bmi :''}</Text>
-            </View>
-          </View>
-
-          <View>
-            <Text>{data.blood_pressure_measurement ? data.blood_pressure_measurement : '' }</Text>
-            <View>
-              <Text>{data.systole ? data.systole.text : '' } : {data.systole ? data.systole.value : '' } mmHg</Text>
-              <Text>{data.diastolic_phase ? data.diastolic_phase.text : '' } : {data.diastolic_phase ? data.diastolic_phase.value : '' } mmHg</Text>
-              <Text>{data.number_of_measurements ? data.number_of_measurements.text : '' } : {data.number_of_measurements ? data.number_of_measurements.value : '' } </Text>
-            </View>
-          </View>
-
-          <View>
-            <Text>{data.blood_pressure_measurement ? data.blood_pressure_measurement : '' }</Text>
-            <View>
-              <Text>{data.urinalysis ? data.urinalysis.text : '' } : {data.urinalysis ? data.urinalysis.value : '' }</Text>
-              <Text>{data.protein ? data.protein.text : '' } : {data.protein ? data.protein.value : '' }</Text>
-              <Text>{data.sugar ? data.sugar.text : '' } : {data.sugar ? data.sugar.value : '' } </Text>
-            </View>
-          </View>
-
-          <View>
-            <Text>{data.blood_lipids ? data.blood_lipids : '' }</Text>
-            <View>
-              <Text>{data.neutral_fat ? data.neutral_fat.text : '' } : {data.neutral_fat ? data.neutral_fat.value : '' } mg/dl</Text>
-              <Text>{data.hdl_cholesterol ? data.hdl_cholesterol.text : '' } : {data.hdl_cholesterol ? data.hdl_cholesterol.value : '' } mg/dl</Text>
-              <Text>{data.ldl_cholesterol ? data.ldl_cholesterol.text : '' } : {data.ldl_cholesterol ? data.ldl_cholesterol.value : '' } mg/dl</Text>
-            </View>
-          </View>
-
-          <View>
-            <Text>{data.liver_function ? data.liver_function : '' }</Text>
-            <View>
-              <Text>GOT : {data.got ? data.got.value : '' } IU/I</Text>
-              <Text>GPT : {data.gpt ? data.gpt.value : '' } IU/I</Text>
-              <Text>Y-GPT : {data.y_gpt ? data.y_gpt.value : '' } IU/I</Text>
-            </View>
-          </View>
-
-          <View>
-            <Text>{data.diabetes_mellitus ? data.diabetes_mellitus : '' }</Text>
-            <View>
-              <Text>{data.blood_glucose_level ? data.blood_glucose_level.text : '' } : {data.blood_glucose_level ? data.blood_glucose_level.value : '' } mg/dl</Text>
-              <Text>glucose level type : {data.blood_glucose_level_type ? data.blood_glucose_level_type : '' }</Text>
-              <Text>{data.hba1c ? data.hba1c.text : '' } {data.hba1c ? data.hba1c.value : '' } %</Text>
-            </View>
-          </View>
-
 					<View>
 						<ImageButton
 	            source={require('../../images/main-menu/check-blood-pressure.png')}
@@ -135,24 +228,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 20,
   },
-  title: {
-    fontSize: 30,
-    marginBottom: 15,
-  },
-  resultContainer: {
+  content: {
     flexDirection: 'row'
   },
-  textarea: {
-  	flex: 1,
-  	height: 500,
-  	borderWidth: 1,
-  	borderColor: "black",
+  dataContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    borderWidth: 1,
+    borderColor: 'black',
   },
   btnImg: {
-  	margin: 10,
-  	marginRight: 0,
-  	padding: 8,
-  	borderColor: 'black',
+    margin: 10,
+    marginRight: 0,
+    padding: 8,
+    borderColor: 'black',
     borderWidth: 1,
     borderRadius: 15,
   },
@@ -160,7 +249,47 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,    
   },
-  text:{
-    fontSize: 15,
+  table: {
+    flex: 1,
+  },
+  tr: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  tdText: {
+    width: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+  },
+  tdValue: {
+    flex: 1,
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 5,
+  },
+  col1: {
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 100,
+  },
+  col2row: {
+    padding: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    width: 140,
+    height: 30,
+  },
+  col3row: {
+    flex: 1,
+    padding: 3,
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 30,
   }
 });
